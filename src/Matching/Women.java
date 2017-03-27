f067daddb1d86159a69136c64652a7e6ca7d620a
Edvard <edvard.jo@gmail.com>
2017-03-27 16:24:15 +0200

    Started on women

---
+++ src/Matching/Women.java
@@ -0,0 +1,17 @@
+package Matching;
+import java.util.Stack;
+
+public class Women {
+
+	int nbr;
+	String name;
+	Stack<Integer> prefs;
+
+	public Women(int nbr, String name, Stack<Integer> prefs){
+		this.nbr = nbr;
+		this.name = name;
+		this.prefs = new Stack<Integer>();
+	}
+	
+	
+}
