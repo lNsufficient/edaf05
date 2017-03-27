c7d0c3e18caa5cce0dd1b0209e86710c6a449f8d
Edvard <edvard.jo@gmail.com>
2017-03-27 16:20:01 +0200

    Kommer igång med matchcouples

---
+++ src/Matching/MatchCouples.java
@@ -0,0 +1,14 @@
+package Matching;
+
+import java.util.LinkedList;
+
+public class MatchCouples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<Integer> menList = new LinkedList<Integer>();
+		
+		
+	}
+
+}
