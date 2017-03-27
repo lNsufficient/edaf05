ebf6a8850245d7097afd81ccbb1f4b27dad81528
Edvard <edvard.jo@gmail.com>
2017-03-27 16:35:25 +0200

    Women improve

--- src/Matching/Women.java
+++ src/Matching/Women.java
@@ -3,14 +3,22 @@ import java.util.Stack;
 
 public class Women {
 
-	int nbr;
-	String name;
-	Stack<Integer> prefs;
+	private int nbr;
+	private String name;
+	private Stack<Integer> prefs;
 
-	public Women(int nbr, String name, Stack<Integer> prefs){
+	public Women(int nbr, String name){
 		this.nbr = nbr;
 		this.name = name;
 		this.prefs = new Stack<Integer>();
+	}
+	
+	public int getNumber(){
+		return nbr;
+	}
+	
+	public String getName(){
+		return name;
 	}
 	
 	
