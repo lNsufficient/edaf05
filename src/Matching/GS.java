d6e7927a13681354115593c7ebaac802717a0126
Eric Sporre <eric.victor.sporre@gmail.com>
2017-03-27 17:07:08 +0200

    First implementation of reading file

--- src/Matching/GS.java
+++ src/Matching/GS.java
@@ -1,12 +1,27 @@
 package Matching;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class GS {
 
 	public static void main(String[] args) {
 
 		//Read the names;
+		File file = new File(args[0]);
+		Scanner scan;
+		try {
+			scan = new Scanner(file);
+			while(scan.hasNextLine()){
+				System.out.println(scan.nextLine());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
 		
 		LinkedList<Integer> menList = new LinkedList<Integer>();
 		
