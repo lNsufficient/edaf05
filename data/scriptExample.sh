445f2b9ad64439fe416fface1269bd01c1f04cec
Edvard <edvard.jo@gmail.com>
2017-03-29 09:40:13 +0200

    fixar data

--- data/scriptExample.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for FILE in *-in.txt
-
-do
-	echo $FILE
-	base=${FILE%-in.txt}
-    java -cp '..'GS $FILE > $base.yourname.out.txt # replace with your command!
-    diff $base.yourname.out.txt $base-out.txt
-done
