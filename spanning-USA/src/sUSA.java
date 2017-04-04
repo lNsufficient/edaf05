d6f309fc610abd58fe6e92bbe6255288c29e7fc2
Edvard <edvard.jo@gmail.com>
2017-04-04 13:40:50 +0200

    started scanner

--- spanning-USA/src/sUSA.java
+++ spanning-USA/src/sUSA.java
@@ -1,2 +1,16 @@
 import java.util.io.scanner;
 
+public class sUSA{
+	
+	
+	public static void main(String args[]) {
+		File file = new File("data/USA-highway-miles.txt");
+		Scanner scan;
+		try {
+			scan = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
